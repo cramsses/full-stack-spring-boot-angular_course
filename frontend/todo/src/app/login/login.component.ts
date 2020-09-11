5192038cf8486e8e96a5d55a467cd0dcc634ad5a
Ramsses <43966422+cramsses@users.noreply.github.com>
2020-09-11 16:08:43 -0500

    Data binding
    Interpolation and event binding

--- frontend/todo/src/app/login/login.component.ts
+++ frontend/todo/src/app/login/login.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username = 'ramsses'
+  password = '1234566'
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  handleLogin(){
+    console.log(this.username)
   }
 
 }
