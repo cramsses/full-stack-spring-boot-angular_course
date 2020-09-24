21945a16964650ff6a9eaab1e7a6e5d0b4e00a10
Ramsses <43966422+cramsses@users.noreply.github.com>
2020-09-23 20:40:55 -0500

    Demo for test maven and spring boot

---
+++ demo-test/sohe-demo/sohe-demo/src/main/java/com/sohe/springboot/web/sohedemo/SoheDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sohe.springboot.web.sohedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoheDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoheDemoApplication.class, args);
+	}
+
+}
---
+++ demo-test/sohe-demo/sohe-demo/src/test/java/com/sohe/springboot/web/sohedemo/SoheDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sohe.springboot.web.sohedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoheDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
