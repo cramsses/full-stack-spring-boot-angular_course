ca3ed31ef96be48afc324c9ca9046596a49c9cc9
Ramsses <43966422+cramsses@users.noreply.github.com>
2020-09-23 23:50:48 -0500

    Restfull web Service

---
+++ backend/restful-web-services/src/main/java/com/sohe/rest/webservices/restfulwebservices/beans/HelloWorldBean.java
@@ -0,0 +1,26 @@
+package com.sohe.rest.webservices.restfulwebservices.beans;
+
+public class HelloWorldBean {
+	
+	private String message;
+
+	public HelloWorldBean(String message) {
+		// TODO Auto-generated constructor stub
+		this.message = message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("HelloWorldBean [message=%s]", message);
+	}
+	
+
+}
