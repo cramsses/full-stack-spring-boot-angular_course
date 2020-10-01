46f078c5a8f1d1a9db72db75384df523c9e3e93c
Ramsses <43966422+cramsses@users.noreply.github.com>
2020-10-01 14:14:56 -0500

    Update from Angular calling WS

--- frontend/todo/src/app/service/data/todo-data.service.ts
+++ frontend/todo/src/app/service/data/todo-data.service.ts
@@ -18,8 +18,15 @@ export class TodoDataService {
   deleteTodo(username:string, id:number){
     return this.http.delete(`http://localhost:8080/users/${username}/todos/${id}`);
   }
+
   retrieveTodo(username:string, id:number){
     return this.http.get<Todo>(`http://localhost:8080/users/${username}/todos/${id}`);
+  }
+
+  updateTodo(username:string, id:number,todo:Todo){
+    return this.http.put(
+      `http://localhost:8080/users/${username}/todos/${id}`
+      ,todo);
   }
 
 }
