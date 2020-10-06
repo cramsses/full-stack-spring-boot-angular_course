fa9bb1b76c3e401106a7dce9853255f7c0ef552a
Ramsses <43966422+cramsses@users.noreply.github.com>
2020-10-06 01:01:20 -0500

    Add HTTP intercpetor as provdier in app.cmponent

--- frontend/todo/src/app/service/http/http-interceptor-basic-auth.service.ts
+++ frontend/todo/src/app/service/http/http-interceptor-basic-auth.service.ts
@@ -1,9 +1,25 @@
+import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class HttpInterceptorBasicAuthService {
+export class HttpInterceptorBasicAuthService implements HttpInterceptor {
 
   constructor() { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler) {
+    let username = 'ramsses';
+    let password = 'dummy';
+    let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+
+    request = request.clone({
+      setHeaders: {
+        Authorization: basicAuthHeaderString
+        }
+      })
+
+    return next.handle(request);
+
+  }
 }
