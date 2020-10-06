b21e28932a3dfa54fb524e586854f4d883645447
Ramsses <43966422+cramsses@users.noreply.github.com>
2020-10-06 02:13:31 -0500

    Create basic-authentication.service.ts

---
+++ frontend/todo/src/app/service/basic-authentication.service.ts
@@ -0,0 +1,50 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BasicAuthenticationService {
+
+  constructor(private http: HttpClient) { }
+
+  authenticate(username, password){
+    console.log('before '+this.isUserLoggedIn());
+    
+    if(username==="ramsses" && password==="dummy"){
+      sessionStorage.setItem('authenticateUser',username);
+      console.log('after '+this.isUserLoggedIn());
+      return true;
+    }
+    return false;
+  }
+
+  executeAuthenticationService(username, password) {
+    //console.log('Executing Hello World Bean Service');
+    
+    
+
+    let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+    
+
+    let headers = new HttpHeaders({
+        Authorization: basicAuthHeaderString
+      })
+    return this.http.get<AuthenticationBean>(`http://localhost:8080/basicauth`,
+    {headers});
+  }
+
+ 
+  isUserLoggedIn(){
+    let user = sessionStorage.getItem('authenticateUser');
+    return !(user === null);
+  }
+
+  logout(){
+    sessionStorage.removeItem('authenticateUser')
+  }
+}
+
+export class AuthenticationBean{
+  constructor(public message : string){}
+}
