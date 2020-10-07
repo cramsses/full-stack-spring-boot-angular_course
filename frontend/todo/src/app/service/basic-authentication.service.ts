96cb5e11eda0a32166552d577fb81d0be846f44f
Ramsses <43966422+cramsses@users.noreply.github.com>
2020-10-07 00:37:20 -0500

    Refactoring basic auth

--- frontend/todo/src/app/service/basic-authentication.service.ts
+++ frontend/todo/src/app/service/basic-authentication.service.ts
@@ -9,40 +9,35 @@ export class BasicAuthenticationService {
 
   constructor(private http: HttpClient) { }
 
-  authenticate(username, password){
-    console.log('before '+this.isUserLoggedIn());
-    
-    if(username==="ramsses" && password==="dummy"){
-      sessionStorage.setItem('authenticateUser',username);
-      console.log('after '+this.isUserLoggedIn());
-      return true;
-    }
-    return false;
-  }
-
   executeAuthenticationService(username, password) {
-    //console.log('Executing Hello World Bean Service');
-    
-    
-
     let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
-    
-
     let headers = new HttpHeaders({
         Authorization: basicAuthHeaderString
       })
-    return this.http.get<AuthenticationBean>(`http://localhost:8080/basicauth`,
+    
+      return this.http.get<AuthenticationBean>(`http://localhost:8080/basicauth`,
     {headers}).pipe(
       map(
         data=>{
           sessionStorage.setItem('authenticateUser',username);
+          sessionStorage.setItem('token',basicAuthHeaderString);
           console.log('Request succeed');
           return data;
         }
       )
     )
+
   }
 
+  getAuthenticatedUser(){
+    return sessionStorage.getItem('authenticateUser');
+  }
+
+  getAuthenticatedToken(){
+    if(this.getAuthenticatedUser){
+      return sessionStorage.getItem('token');
+    }
+  }
  
   isUserLoggedIn(){
     let user = sessionStorage.getItem('authenticateUser');
@@ -50,7 +45,8 @@ export class BasicAuthenticationService {
   }
 
   logout(){
-    sessionStorage.removeItem('authenticateUser')
+    sessionStorage.removeItem('authenticateUser');
+    sessionStorage.removeItem('token');
   }
 }
 
